cf47181cfd97440e65ceaad4b0c18278a03f261b
Abel-19 <yao-abel.assagba@ism.edu.sn>
2020-04-26 22:00:39 +0000

    Tout les fichiers PHP

---
+++ src/fr/abel/rectangle/Rectangle.java
@@ -0,0 +1,18 @@
+package fr.abel.rectangle;
+class Main {
+   public static void main (String[] args)
+   {
+     System.out.print("Entrez la longueur du rectangle: ");
+     double longueur = nextDouble();
+     
+     System.out.print("Entrez la largeur du rectangle: ");
+     double largeur = nextDouble();
+     double surface = longueur * largeur;
+     
+     System.out.println("La surface du rectangle est : "+surface);
+   }
+
+private static double nextDouble() {
+	return 0;
+}
+}
---
+++ src/fr/abel19/helloword/main.java
@@ -0,0 +1,10 @@
+package fr.abel19.helloword;
+
+public class main {
+
+	public main(String[] args) {
+		System.out.println("Hello Word");
+
+	}
+
+}
